f305099bda10a1c31901449060cb4c30136efe65
ElonBrown <f1570053668@163.com>
2019-03-11 22:52:58 +0800

    Only Centos

--- ssr_ServerSpeeder/ssr_ServerSpeeder.sh
+++ ssr_ServerSpeeder/ssr_ServerSpeeder.sh
@@ -4,6 +4,6 @@ wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/Shado
 bash serverspeeder-v.sh CentOS 7.1 3.10.0-229.1.2.el7.x86_64 x64 3.11.20.4 serverspeeder_3283
 
 # install ShadowsocksR
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/ssr.sh
 chmod +x ssr.sh
 bash ssr.sh
