6e1fcffb1567cce4be68cebba64ea3f301807cf9
ElonBrown <f1570053668@163.com>
2019-03-11 22:52:35 +0800

    Debian and Centos

--- ssr_bbr/ssr_bbr.sh
+++ ssr_bbr/ssr_bbr.sh
@@ -1,6 +1,7 @@
 # Debian and Centos
 # install ShadowsocksR
-wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/ssr.sh
+chmod +x ssr.sh
 bash ssr.sh
 
 # install bbr
