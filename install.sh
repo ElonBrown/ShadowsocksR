5505aff666b67cabbd1e3f4e93937e3f899dc4fd
ElonBrown <f1570053668@163.com>
2019-03-12 00:35:46 +0800

    Auto Install

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ else
     release=""
 fi
 echo "当前系统为：${release}"
+
 if [[ x"${release}" == x"centos" ]]; then
     read -p "选择要安装的加速器ServerSpeeder or bbr? [s/b]" speeder
     if [[ ${speeder} == "b" || ${speeder} == "B" ]]; then
@@ -29,9 +30,15 @@ if [[ x"${release}" == x"centos" ]]; then
     else
         kernel=`uname -r`
         if [[ x"${kernel}" == x"3.10.0-229.1.2.el7.x86_64" ]]; then
-            wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/ssr_ServerSpeeder.sh
-            bash ssr_ServerSpeeder.sh
+            # wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/ssr_ServerSpeeder.sh
+            # bash ssr_ServerSpeeder.sh
+            wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/serverspeeder-v.sh
+            bash serverspeeder-v.sh CentOS 7.1 3.10.0-229.1.2.el7.x86_64 x64 3.11.20.4 serverspeeder_3283
         else
+            wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/ssr.sh
+            chmod +x ssr.sh
+            bash ssr.sh
+
             wget -N --no-check-certificate https://raw.githubusercontent.com/ElonBrown/ShadowsocksR/master/ssr_ServerSpeeder/centos_replace_kernel.sh
             bash centos_replace_kernel.sh
         fi
